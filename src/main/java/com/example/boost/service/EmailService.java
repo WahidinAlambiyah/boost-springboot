40fa1d64e678ce214f7979b2f0e59e72bb891150
WahidinAlambiyah <wahidin.alambiyah@gmail.com>
2025-12-03 12:12:36 +0700

    Configure SMTP email delivery

--- src/main/java/com/example/boost/service/EmailService.java
+++ src/main/java/com/example/boost/service/EmailService.java
@@ -2,6 +2,9 @@ package com.example.boost.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +12,25 @@ public class EmailService {
 
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
 
+    private final JavaMailSender mailSender;
+    private final String fromAddress;
+
+    public EmailService(JavaMailSender mailSender,
+                        @Value("${spring.mail.username:}") String fromAddress) {
+        this.mailSender = mailSender;
+        this.fromAddress = fromAddress;
+    }
+
     public void sendEmail(String to, String subject, String body) {
-        log.info("Simulating email send to {} with subject '{}': {}", to, subject, body);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        if (fromAddress != null && !fromAddress.isBlank()) {
+            message.setFrom(fromAddress);
+        }
+
+        mailSender.send(message);
+        log.info("Email sent to {} with subject '{}'", to, subject);
     }
 }
