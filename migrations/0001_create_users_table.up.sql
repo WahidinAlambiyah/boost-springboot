30569e9d933f829fe3774fea79e60c10ce442306
WahidinAlambiyah <wahidin.alambiyah@gmail.com>
2026-01-23 16:31:31 +0700

    Use pgcrypto uuid generation in migration

--- migrations/0001_create_users_table.up.sql
+++ migrations/0001_create_users_table.up.sql
@@ -1,7 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     full_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     username VARCHAR(100) NOT NULL,
