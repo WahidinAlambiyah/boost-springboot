5e9f7b58153cafc74319a4bf3222c09d41ea8ba0
WahidinAlambiyah <wahidin.alambiyah@gmail.com>
2026-05-10 21:15:11 +0700

    Export TailAdmin dropdowns and icons

--- frontend/src/app/components/tailadmin/index.ts
+++ frontend/src/app/components/tailadmin/index.ts
@@ -1,3 +1,6 @@
 export { TailAdminBadge } from "./tailadmin-badge";
 export { TailAdminButton } from "./tailadmin-button";
 export { TailAdminCard } from "./tailadmin-card";
+export { TailAdminNotificationDropdown } from "./tailadmin-notification-dropdown";
+export { TailAdminUserDropdown } from "./tailadmin-user-dropdown";
+export * from "./tailadmin-icons";
