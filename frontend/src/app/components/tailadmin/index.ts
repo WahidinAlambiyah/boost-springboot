ebec1d62847798a309e759cffe073fd4a35a043b
WahidinAlambiyah <wahidin.alambiyah@gmail.com>
2026-05-11 10:02:24 +0700

    Export TailAdmin confirmation modal

--- frontend/src/app/components/tailadmin/index.ts
+++ frontend/src/app/components/tailadmin/index.ts
@@ -1,6 +1,7 @@
 export { TailAdminBadge } from "./tailadmin-badge";
 export { TailAdminButton } from "./tailadmin-button";
 export { TailAdminCard } from "./tailadmin-card";
+export { TailAdminConfirmModal } from "./tailadmin-confirm-modal";
 export { TailAdminNotificationDropdown } from "./tailadmin-notification-dropdown";
 export { TailAdminUserDropdown } from "./tailadmin-user-dropdown";
 export * from "./tailadmin-icons";
